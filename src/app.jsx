90d5be0763010100721406968c3b69f38fb08ae1
Ryan Carniato <ryansolid@gmail.com>
2019-11-17 20:49:10 -0800

    update solid

--- src/app.jsx
+++ src/app.jsx
@@ -8,7 +8,7 @@ const App = () => {
       const value = ENV.generateData().toArray();
       // Cheat like top libraries
       Promise.resolve(value).then(v =>
-        setState(reconcile(["databases", v], { merge: true, key: null }))
+        setState("databases", reconcile(v, { merge: true, key: null }))
       );
       setTimeout(load, ENV.timeout);
     };
